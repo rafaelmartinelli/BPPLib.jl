54514f3076c48151bba20ce007ce478a8169e9c3
Rafael Martinelli <martinelli@puc-rio.br>
2021-04-28 18:25:57 -0300

    Unit tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
 using BPPLib
 using Test
 
-@testset "BPPLib.jl" begin
-    # Write your tests here.
+@testset "LoadSymbol" begin
+    data = loadBPP(:Falkenauer_u1000_02)
+    @test length(data.weights) == 1000
+    @test data.capacity == 150
+    @test data.name == "Falkenauer_u1000_02"
+end
+
+@testset "ErrorSymbol" begin
+    data = loadBPP(:Falkenauer_u1000_0)
+    @test data === nothing
+end
+
+@testset "LoadString" begin
+    data = loadBPP(joinpath(@__DIR__, "../data/Falkenauer_u120_01.txt"))
+    @test length(data.weights) == 120
+    @test data.capacity == 150
+    @test data.name == "Falkenauer_u120_01"
+end
+
+@testset "ErrorString" begin
+    data = loadBPP(joinpath(@__DIR__, "../data/Falkenauer_u120_0.txt"))
+    @test data === nothing
+end
+
+@testset "Show" begin
+    data = loadBPP(:Falkenauer_u1000_02)
+    @test_nowarn Base.show(data)
 end
